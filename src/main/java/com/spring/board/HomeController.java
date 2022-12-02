905f69084998da38ea0f41abfd193020abc25bc1
Tamin Lee <taemin5161@gmail.com>
2022-12-02 23:10:28 +0900

    Initial Commit

---
+++ src/main/java/com/spring/board/HomeController.java
@@ -0,0 +1,24 @@
+package com.spring.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        SimpleDateFormat dateFormat =
+                new SimpleDateFormat("yyyy년 M월 d일 aa h시 m분 s초 z");
+        Date now = new Date();
+        String formattedDate = dateFormat.format(now);
+
+        model.addAttribute("serverTime", formattedDate);
+
+        return "index";
+    }
+}
