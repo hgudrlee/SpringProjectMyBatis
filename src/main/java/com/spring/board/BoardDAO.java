c2bd73485e45a869c664d4739b29af42c081600f
Tamin Lee <taemin5161@gmail.com>
2022-12-09 21:06:09 +0900

    Initial Commit

--- src/main/java/com/spring/board/BoardDAO.java
+++ src/main/java/com/spring/board/BoardDAO.java
@@ -1,5 +1,6 @@
 package com.spring.board;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,46 +14,29 @@ import java.util.List;
 @Repository
 public class BoardDAO {
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
-	private final String BOARD_INSERT = "insert into BOARD (title, writer, content) values (?,?,?)";
-	private final String BOARD_UPDATE = "update BOARD set title=?, writer=?, content=? where seq=?";
-	private final String BOARD_DELETE = "delete from BOARD  where seq=?";
-	private final String BOARD_GET = "select * from BOARD  where seq=?";
-	private final String BOARD_LIST = "select * from BOARD order by seq desc";
+	SqlSession sqlSession;
 
 	public int insertBoard(BoardVO vo) {
-		return jdbcTemplate.update(BOARD_INSERT, new
-				Object[]{vo.getTitle(), vo.getWriter(), vo.getContent()});
+		int result = sqlSession.insert("Board.insertBoard", vo);
+		return result;
 	}
 
 	public int deleteBoard(int id) {
-		return jdbcTemplate.update(BOARD_DELETE,
-				new Object[]{id});
+		int result = sqlSession.delete("Board.insertBoard", id);
+		return result;
 	}
 	public int updateBoard(BoardVO vo) {
-		return jdbcTemplate.update(BOARD_UPDATE,
-				new Object[]{vo.getTitle(), vo.getWriter(), vo.getContent(), vo.getSeq()});
-	}	
-	
+		int result = sqlSession.update("Board.insertBoard", vo);
+		return result;
+	}
+
 	public BoardVO getBoard(int seq) {
-		return jdbcTemplate.queryForObject(BOARD_GET,
-				new Object[] {seq},
-				new BeanPropertyRowMapper<BoardVO>(BoardVO.class));
+		BoardVO one = sqlSession.selectOne("Board.getBoard", seq);
+		return one;
 	}
 	
 	public List<BoardVO> getBoardList(){
-		return jdbcTemplate.query(BOARD_LIST, new RowMapper<BoardVO>() {
-			@Override
-			public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-				BoardVO data = new BoardVO();
-				data.setSeq(rs.getInt("seq"));
-				data.setTitle(rs.getString("title"));
-				data.setContent(rs.getString("content"));
-				data.setRegdate(rs.getDate("regdate"));
-				data.setWriter(rs.getString("writer"));
-				return data;
-			}
-		});
+		List<BoardVO> list = sqlSession.selectList("Board.getBoardList");
+		return list;
 	}
 }
